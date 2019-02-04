0109412eff32d36fc4641a1389261a678e0d854a
Hao-Lam Chan <chanh101@coventry.ac.uk>
2019-02-04 02:43:38 +0000

    added hp

--- TheOnslaughtOfFerocia/hlc.cpp
+++ TheOnslaughtOfFerocia/hlc.cpp
@@ -7,21 +7,39 @@
 using namespace std;
 
 
-void combat( std::string enemy){
-    string move;
+void combat(int Hatk ,string Ename, int EHp){
+    int MaxEHp = EHp;
     cout << "You encounter a(n) ";
-    cout << enemy << endl;
-    cout << "What would you like to do?" << endl;
-    cout << "1. atk " << endl;
-    cin >> move;
-    if (move == "atk" || move == "1"){
-        cout << "You attack the " ;
-        cout << enemy << endl;
+    cout << Ename << endl;
+    
+    while (EHp > 0){
+        string move;        
+        cout << "What would you like to do?" << endl;
+        cout << "1. atk " << endl;
+        cin >> move;
+    
+        if (move == "atk" || move == "1"){
+            EHp = EHp - Hatk;
+            cout << "You attack the " ;
+            cout << Ename ;
+            cout << "  HP: ";
+            cout << EHp;
+            cout << "/";
+            cout << MaxEHp << endl;
+        }
+        else{
+            cout << "That is not an option" << endl;
+        }
     }
+    cout << "The ";
+    cout << Ename;
+    cout << " was defeated" << endl;
 }
-
 int main(){
-    string enemy = "orc";
-    combat(enemy);
+    int heroatk = 5;
+    string enemyname = "orc";
+    int enemyhp = 10;
+    
+    combat(heroatk,enemyname,enemyhp);
     return 0;
 }
