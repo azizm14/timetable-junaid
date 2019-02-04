102358e4fbeb80e4a29b1e086c452131bacdc447
Hao-Lam Chan <chanh101@coventry.ac.uk>
2019-02-04 02:49:11 +0000

    comments

--- TheOnslaughtOfFerocia/hlc.cpp
+++ TheOnslaughtOfFerocia/hlc.cpp
@@ -7,18 +7,18 @@
 using namespace std;
 
 
-void combat(int Hatk ,string Ename, int EHp){
+void combat(int Hatk ,string Ename, int EHp){   //start of combat function
     int MaxEHp = EHp;
     cout << "You encounter a(n) ";
     cout << Ename << endl;
     
-    while (EHp > 0){
-        string move;        
+    while (EHp > 0){                            //loop until enemy is dead
+        string move;                            //deciding what the player does
         cout << "What would you like to do?" << endl;
         cout << "1. atk " << endl;
         cin >> move;
     
-        if (move == "atk" || move == "1"){
+        if (move == "atk" || move == "1"){       //start of atk
             EHp = EHp - Hatk;
             cout << "You attack the " ;
             cout << Ename ;
@@ -35,11 +35,11 @@ void combat(int Hatk ,string Ename, int EHp){
     cout << Ename;
     cout << " was defeated" << endl;
 }
-int main(){
-    int heroatk = 5;
+int main(){                                //main function, currently used to store stats as global variables
+    int heroatk = 5;                       //stats should be stored in database later
     string enemyname = "orc";
     int enemyhp = 10;
     
-    combat(heroatk,enemyname,enemyhp);
+    combat(heroatk,enemyname,enemyhp);     //call combat function
     return 0;
 }
