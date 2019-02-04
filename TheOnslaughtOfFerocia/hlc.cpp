927de0aae3b418262a88ccbf76eeac89b0c7d79a
Hao-Lam Chan <chanh101@coventry.ac.uk>
2019-02-04 03:18:27 +0000

    added health bar

--- TheOnslaughtOfFerocia/hlc.cpp
+++ TheOnslaughtOfFerocia/hlc.cpp
@@ -7,25 +7,43 @@
 using namespace std;
 
 
-void combat(int Hatk ,string Ename, int EHp){   //start of combat function
-    int MaxEHp = EHp;
+void combat(int Hhp,int Hatk,string Ename,int Ehp,int Eatk){   //start of combat function
+    int MaxHhp = Hhp;
+    int MaxEhp = Ehp;
+    cout << endl;
     cout << "You encounter a(n) ";
     cout << Ename << endl;
+    cout << endl;
     
-    while (EHp > 0){                            //loop until enemy is dead
+    while (Ehp > 0){                            //loop until enemy is dead
         string move;                            //deciding what the player does
+        cout << "Hero health: ";
+        cout << Hhp;
+        cout << "/";
+        cout << MaxHhp;
+        cout << "       ";
+        cout << Ename;
+        cout << " health: ";
+        cout << Ehp;
+        cout << "/";
+        cout << MaxEhp << endl;
+        cout << endl;
+        
+        
         cout << "What would you like to do?" << endl;
         cout << "1. atk " << endl;
+        cout << "2. intimidate" << endl;
         cin >> move;
     
         if (move == "atk" || move == "1"){       //start of atk
-            EHp = EHp - Hatk;
+            Ehp = Ehp - Hatk;
             cout << "You attack the " ;
             cout << Ename ;
             cout << "  HP: ";
-            cout << EHp;
+            cout << Ehp;
             cout << "/";
-            cout << MaxEHp << endl;
+            cout << MaxEhp << endl;
+            cout << endl;
         }
         else{
             cout << "That is not an option" << endl;
@@ -36,10 +54,13 @@ void combat(int Hatk ,string Ename, int EHp){   //start of combat function
     cout << " was defeated" << endl;
 }
 int main(){                                //main function, currently used to store stats as global variables
-    int heroatk = 5;                       //stats should be stored in database later
-    string enemyname = "orc";
-    int enemyhp = 10;
+    int herohp = 20;                       //stats should be stored in database later
+    int heroatk = 5;                       
     
-    combat(heroatk,enemyname,enemyhp);     //call combat function
+    string enemyname = "Orc";
+    int enemyhp = 10;
+    int enemyatk = 8;
+    
+    combat(herohp,heroatk,enemyname,enemyhp,enemyatk);     //call combat function
     return 0;
 }
