45307b59a73548342825be1c693dc7adfef693dc
Hao-Lam Chan <chanh101@coventry.ac.uk>
2019-02-04 02:03:38 +0000

    attack

--- TheOnslaughtOfFerocia/hlc.cpp
+++ TheOnslaughtOfFerocia/hlc.cpp
@@ -8,13 +8,20 @@ using namespace std;
 
 
 void combat( std::string enemy){
+    string move;
     cout << "You encounter a(n) ";
     cout << enemy << endl;
     cout << "What would you like to do?" << endl;
+    cout << "1. atk " << endl;
+    cin >> move;
+    if (move == "atk" || move == "1"){
+        cout << "You attack the " ;
+        cout << enemy << endl;
+    }
 }
 
 int main(){
-     std::string enemy = "orc";
+    string enemy = "orc";
     combat(enemy);
     return 0;
 }
