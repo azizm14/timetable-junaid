a6f3bce6916821a627bda4a05db79bcf12d75710
Sandeep Beedh (beedhs) <beedhs@coventry.ac.uk>
2019-02-17 22:20:28 +0000

    Create outoftown2.cpp

---
+++ TheOnslaughtOfFerocia/outoftown2.cpp
@@ -0,0 +1,187 @@
+
+void out_town() {
+	string choice_0;
+  
+	cout << "OUT OF TOWN" << endl;
+
+	cout << "You have decided to head out of town...." << endl;
+	cout << "as you are walking you realise that you start to feel dizzy" << endl;
+	cout << " suddenly you collaps..." << endl;
+	cout << "waking up later you find out that you are in a deserted park there is no life to be seen" << endl;
+	cout << "you decide to walk forward on coming you see" << endl;
+  {
+    std::cout << R"(    
+
+
+        _    .  ,   .           .
+    *  / \_ *  / \_      _  *        *   /\'__        *
+      /    \  /    \,   ((        .    _/  /  \  *'.
+ .   /\/\  /\/ :' __ \_  `          _^/  ^/    `--.
+    /    \/  \  _/  \-'\      *    /.' ^_   \_   .'\  *
+  /\  .-   `. \/     \ /==~=-=~=-=-;.  _/ \ -. `_/   \
+ /  `-.__ ^   / .-'.--\ =-=~_=-=~=^/  _ `--./ .-'  `-
+/      `.  / /       `.~-^=-=~=^=.-'      '-._ `._
+
+        
+        )" ;
+}
+
+  
+  
+	cout << "a forest towards you left and a swamp towards your right, which do you choose: " << endl;
+  cout << "Please type Forest or Swamp " << endl;
+	cin >> choice_11;
+  
+  
+	if (choice_11 == "Forest" || choice_11 == "forest" || choice_11 == "FOREST" || choice_11 == "for") {
+		cout << "As you are walking into the forest you can smell the fresh breaze of air..." << endl;
+		cout << "you can also hear the birds over head....." << endl;
+    
+    
+     {
+    std::cout << R"(    
+
+
+                            /"  /"  /";
+                           /  /`  /` /
+                          / /  / ` /`
+                         /         ~;
+                        <           ;
+                         \           ;
+                          ;          ;
+                          ;          ;
+                         ;          /   /^\
+                     ___/-,.   __ /`  /`   \
+      /~a`~-,__,_,-~`         "`~~-_ `     |
+"""""`-___,,.,_,__                    ~-,_ }
+                  `~`~""`~-,_          _)_`~`~-,__ _-\,
+                     ;`      `~~`\~"``"  "~`~~`~~```""`
+                    /             ;  `\,/`
+                   |             ;
+                   ;            ;
+                  /            ;
+                  `|_          ;
+                    `|         ;
+                     ;  \ \ \ ;
+                     ;|`\; \\``
+                     ``  "  "
+        )" ;
+}
+    
+    
+    
+    
+    
+		cout << "everything seems to be going fine you are enjoying atmosphere" << endl;
+		cout << "however you accidently knock over a stone this triggers the ground to rumble" << endl;
+		cout << "you start to panic and therfore decide to....." << endl;
+		cout << "run back to where you came from, you now have an option do you wish to go the swamp instead" << endl;
+    cout << "Please type Yes or No" << endl;
+    cin >> choice_22;
+	}
+
+  
+	// this option will give the use of combat code to be implemnted 
+
+	if (choice_22 == "Yes" || choice_22 == "yes" || choice_22 == "Yes" || choice_22 == "yesss") 
+  {
+		cout << "You have now entered a swamp, you look around all you can see " << endl;
+		cout << "is leaves and bones on the floor it all looks unusal to you" << endl;
+    cout << "as you walk forward towards the waters you meet a hunched over creature it looks at you" << endl;
+    cout << "and snarls, you realise the gollum" << endl;
+     {
+    std::cout << R"(    
+
+
+           ___
+         .';:;'.
+        /_' _' /\   __
+        ;a/ e= J/-'"  '.
+        \ ~_   (  -'  ( ;_ ,.
+         L~"'_.    -.  \ ./  )
+         ,'-' '-._  _;  )'   (
+       .' .'   _.'")  \  \(  |
+      /  (  .-'   __\{`', \  |
+     / .'  /  _.-'   "  ; /  |
+    / /    '-._'-,     / / \ (
+ __/ (_    ,;' .-'    / /  /_'-._
+`"-'` ~`  ccc.'   __.','     \j\L\
+                 .='/|\7      
+                ' `
+
+
+
+  
+        )" ;
+}
+    
+    
+    cout << "is aggravated it wants to fight" << endl;
+    cout << "do you wish to attack " << endl;
+
+ 
+    cin >> choice_33;
+// this part is for attacking Hao-lam to add his combat section
+    
+	}
+  
+	else {
+
+		cout << "That is unfortunately not an option" << endl;
+	}
+	// End the game 
+	
+  
+	 
+	//They dont wish to go to the swamp which means they must prepare for the next war
+	if (choice_33 == "Yes" || choice_33 == "yes" || choice_33 == "Yes" || choice_33 == "yesss") 
+    
+    
+		cout << "you've managed to kill the gollum" << endl;
+		cout << "however after killing the gollum the sky falls dark" << endl;
+		cout << "many more creatures start to approach you" << endl;
+	}
+
+     {
+    std::cout << R"(    
+
+######################~~..'|.##############.|`..~~#######################
+##############~./`.~~./' ./ ################ \. `\. ~~.`\.~##############
+############~.' `.`-'   /   ~#############~ .  \   `-'.'  `.~############
+##########~.'    |     |  .'\ ~##########~ /`.  |     |     `.~##########
+########~.'      |     |  |`.`._ ~####~ _.'.'|  |     |       `.~########
+######~.'        `.    |  `..`._|\.--./|_.'..'  |    .'         `.~######
+####~.'            \   | #.`.`._`.'--`.'_.'.'.# |   /             `.~####
+##~.'       ......  \  | ###.`~'(o\||/o)`~'.### |  /  ......        `.~##
+~.`.......'~      `. \  \~####  |\_  _/|  ####~/  / .'      ~`........'.~
+;.'                 \ .----.__.'`(n||n)'`.__.----. /                  `;
+`.                  .'    .'   `. \`'/ .'   `.    `.                  .'
+#:               ..':      :    '. ~~ .`    :      :`..               :#
+#:             .'   :     .'     .'  `.     `.     :   `.             :#
+#:           .'    .`   .'       . || .       `.   '.    `.           :#
+#:         .'    .' :       ....'      `....       : `.    `.         :#
+#:       .'    .' ) )        (      )     (      (    )`.    `.       :#
+#:     ..'    .  ( ((   )  ) )) (  ((  (  ))  )  ))  ((  `.   `..     :#
+#:  ..'      .'# ) ) ) (( ( ( (  ) ) ) ))( ( (( ( (  ) ) #`.     `..  :#
+#;.'        .'##|((  ( ) ) ) ) )( (  (( ( ) )) ) ) )( (||##`.       `.:#
+#`.        .'###|\__  )( (( ( ( )  )  )) )  (  (( ( )_)/|###`.       .'#
+##.`       '#####\__~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~__/#####`      '.##
+###        #######  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #######       ###
+
+        )" ;
+}
+
+
+  
+}
+
+
+// Comments on the next war that they are going to 
+
+
+{
+  cout << "After realising that they are many creatures from unknown sources you have decided that you are going to go back in to the town" << endl;
+	cout << "and grow strong and prepare for the war " << endl;
+	cout << "you are now back in the main town....." << endl;
+
+}
