8efee4ac0c08f4a712d76ebd4d2d30d1fd3291a7
Luke Charles (charle44) <charle44@coventry.ac.uk>
2019-02-18 09:45:51 +0000

    updated buggy code

--- TheOnslaughtOfFerocia/out_town2.cpp
+++ TheOnslaughtOfFerocia/out_town2.cpp
@@ -5,29 +5,20 @@
 
 using namespace std;	
 
-void out_town() 
-
-{
+void out_town(){
   
-string choice11,choice_22,choice_33 ;
-  
-  
- choice_0 
-  choice_11
+string choice_33;
+int choice_11 = 0, choice_22;
  
   
 	cout << "OUT OF TOWN" << endl;
 
 	cout << "You have decided to head out of town...." << endl;
 	cout << "as you are walking you realise that you start to feel dizzy" << endl;
-	cout << " suddenly you collaps..." << endl;
+	cout << " suddenly you collapse..." << endl;
 	cout << "waking up later you find out that you are in a deserted park there is no life to be seen" << endl;
 	cout << "you decide to walk forward on coming you see" << endl;
- 
-  {
-    std::cout << R"(    
-
-
+  std::cout << R"(    
         _    .  ,   .           .
     *  / \_ *  / \_      _  *        *   /\'__        *
       /    \  /    \,   ((        .    _/  /  \  *'.
@@ -36,32 +27,18 @@ string choice11,choice_22,choice_33 ;
   /\  .-   `. \/     \ /==~=-=~=-=-;.  _/ \ -. `_/   \
  /  `-.__ ^   / .-'.--\ =-=~_=-=~=^/  _ `--./ .-'  `-
 /      `.  / /       `.~-^=-=~=^=.-'      '-._ `._
-
         
         )" ;
-  }
-
   
-{
 	cout << "a forest towards you left and a swamp towards your right, which do you choose: " << endl;
-  cout << "Please type Forest or Swamp " << endl;
+  cout << "1. Forest\n2. Swamp " << endl;
+  cin >> choice_11;
   
-}
-    
-{
-	cin >> choice_11;
-
-  
-	if (choice_11 == "Forest" || choice_11 == "forest" || choice_11 == "FOREST" || choice_11 == "for")
+	if (choice_11 == 1)
   {
-		cout << "As you are walking into the forest you can smell the fresh breaze of air..." << endl;
+		cout << "As you are walking into the forest you can smell the fresh breeze of air..." << endl;
 		cout << "you can also hear the birds over head....." << endl;
-  }
-    
-     {
     std::cout << R"(    
-
-
                             /"  /"  /";
                            /  /`  /` /
                           / /  / ` /`
@@ -86,34 +63,20 @@ string choice11,choice_22,choice_33 ;
                      ;|`\; \\``
                      ``  "  "
         )" ;
-}
-    
-    
-  {
 		cout << "everything seems to be going fine you are enjoying atmosphere" << endl;
 		cout << "however you accidently knock over a stone this triggers the ground to rumble" << endl;
-		cout << "you start to panic and therfore decide to....." << endl;
+		cout << "you start to panic and therefore decide to....." << endl;
 		cout << "run back to where you came from, you now have an option do you wish to go the swamp instead" << endl;
     cout << "Please type Yes or No" << endl;
+    cout << "1. Yes\n2. No" << endl;
     cin >> choice_22;
-	}
-
 	// this option will give the use of combat code to be implemnted 
-
-	if (choice_22 == "Yes" || choice_22 == "yes" || choice_22 == "Yes" || choice_22 == "yesss") 
-    
-  {
-   
+	if (choice_22 == 1){
 		cout << "You have now entered a swamp, you look around all you can see " << endl;
 		cout << "is leaves and bones on the floor it all looks unusal to you" << endl;
     cout << "as you walk forward towards the waters you meet a hunched over creature it looks at you" << endl;
-    cout << "and snarls, you realise the gollum" << endl;
-      
-  }
-     {
+    cout << "and snarls, you realise it's gollum" << endl;
     std::cout << R"(    
-
-
            ___
          .';:;'.
         /_' _' /\   __
@@ -130,37 +93,27 @@ string choice11,choice_22,choice_33 ;
                  .='/|\7      
                 ' `
         )" ;
-}
-  {
-    
     cout << "is aggravated it wants to fight" << endl;
     cout << "do you wish to attack " << endl;
-  }
- 
     cin >> choice_33;
 // this part is for attacking Hao-lam to add his combat section
+    cout << "you've managed to kill the gollum" << endl;
+		cout << "however after killing the gollum the sky falls dark" << endl;
+		cout << "many more creatures start to approach you" << endl;
     
-	}
-  
-	else {
-
+	}else{
 		cout << "That is unfortunately not an option" << endl;
-	}
+	}}
 	// End the game 
 	
   
 	//They dont wish to go to the swamp which means they must prepare for the next war
-	if (choice_33 == "Yes" || choice_33 == "yes" || choice_33 == "Yes" || choice_33 == "yesss") 
-    
+	if (choice_11 == 2)   
   {
-    
-		cout << "you've managed to kill the gollum" << endl;
-		cout << "however after killing the gollum the sky falls dark" << endl;
-		cout << "many more creatures start to approach you" << endl;
-	}
-  
+    cout << "Instead of going to the swamp..." << endl;
+    //you prepare for the next war
+    //IF STATEMENT NEEDS COMPLETING
     std::cout << R"(    
-
 ######################~~..'|.##############.|`..~~#######################
 ##############~./`.~~./' ./ ################ \. `\. ~~.`\.~##############
 ############~.' `.`-'   /   ~#############~ .  \   `-'.'  `.~############
@@ -183,15 +136,21 @@ string choice11,choice_22,choice_33 ;
 #`.        .'###|\__  )( (( ( ( )  )  )) )  (  (( ( )_)/|###`.       .'#
 ##.`       '#####\__~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~__/#####`      '.##
 ###        #######  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #######       ###
-
         )" ;
 
 // Comments on the next war that they are going to 
 
-{
+
 
   cout << "After realising that they are many creatures from unknown sources you have decided that you are going to go back in to the town" << endl;
 	cout << "and grow strong and prepare for the war " << endl;
 	cout << "you are now back in the main town....." << endl;
+  }
 }
+
+int main(){
+  out_town();
+}
+
+
 
