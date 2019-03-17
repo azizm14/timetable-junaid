dbed4eb256300768a953b9d90acb68551dc777b3
Sandeep Beedh (beedhs) <beedhs@coventry.ac.uk>
2019-03-17 19:49:00 +0000

    Create credits.cpp
    ending of the game

---
+++ TheOnslaughtOfFerocia/credits.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+cout << "Askii art has been used from" << endl;
+cout << "https://www.asciiart.eu/" << endl;
+cout << "huge thanks to them!!"<<endl;
+cout <<"Code creaters"<<endl;
+cout<<"1.sunny - out of town\n 2. luke main methods\n 3.Hoa-lam - Combat sections\n 4. hamaza databases";
+return 0;
+}
+
+// feel free to eddits this part 
