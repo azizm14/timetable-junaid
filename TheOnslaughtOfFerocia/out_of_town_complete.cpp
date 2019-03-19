e8a578d08f19ad313e16a5a39dfd29fda55b777c
Sandeep Beedh (beedhs) <beedhs@coventry.ac.uk>
2019-03-19 11:51:08 +0000

    Sandeep_out_of_town_quest.cpp

--- TheOnslaughtOfFerocia/out_of_town_complete.cpp
+++ TheOnslaughtOfFerocia/out_of_town_complete.cpp
@@ -9,8 +9,9 @@ using namespace std;
 
 class Art{
   public:
+  //accessable anywhere - public
   void printLandscape(){
-    //we made four methods to call the ASCII art for the project
+    //Four methods to call the ASCII art for the project
     std::cout << R"(    
         _    .  ,   .           .
     *  / \_ *  / \_      _  *        *   /\'__        *
@@ -124,64 +125,48 @@ int choice_11 = 0, choice_22, choice_33,age;
             break;  // end of case will run defaul only
         default:
             cout << "sorry this is'nt a available option n" << endl;
-
     }
-
 }
-    
     {
 			cout << "You look around all you can see....... " << endl;
 			cout << "is leaves and bones on the floor it all looks unusal to you" << endl;
 			cout << "as you walk forward towards the waters you meet some hunched back over looking creatures they look at you" << endl;
 			cout << " thye snarl, you realise that they are gollums" << endl;
       cout<< "you actually realise that they are a few, i can inform they are of ages:" << endl;
-      
-      {
-        
+      {    
     srand(time(0)); // a random number from the clock - every time game played ensures new number generated
 
     for (int x = 1; x<5;x++){
         cout << 1+(rand()%10)<<"years old"<< endl;
       // using a rand generator but i cant have zero age so i added 1 
-      // decrease the age i used modulus function 
+      // decrease the age using modulus function 
     }
-
 }
-
 			callArt.gollum();
 
 			cout << "they are aggravated and want to fight" << endl;
-			cout << "do you wish to attack " << endl;
-      
-		attack_choice:
-      
+			cout << "do you wish to attack " << endl; 
+		  attack_choice:
 			cout << "please choose 1 = yes 2 = no" << endl;
-      
 			cin >> choice_33;
-
+      
 			if (choice_33 == 1) {
 //the file combat_start is called here.
 				Art run;
 			//	run.Combat_Start();
 				//test();
 				//weapons();
+				// uncomment to call .h file will allow combat to run
 				cout << "you've managed to kill the gollum" << endl;
 				cout << "however after killing the gollum the sky falls dark" << endl;
-				cout << "many more creatures start to approach you" << endl;
-        
-          
+				cout << "many more creatures start to approach you" << endl;   
 {
- 
     int radoms ;
     int creatares[22];
-
     srand(time(0)); 
-
     for (int x = 1; x<22;x++) {
         radoms =  1+(rand()%14); 
     }
-
-  
     cout << "1.Amount of Creatures remaining\n2.Hits to kill the Creatures"<<endl;
         cout << "some creatures with huge hitpoints can not be killed they must be convinsed to change sides"<<endl;
 
@@ -190,21 +175,17 @@ int choice_11 = 0, choice_22, choice_33,age;
         creatares[13]  = radoms;
       
        cout << x << "   ------------  " << creatares[x] << endl;
-      
     }
 }
-
-        
- 
 				cout << "therefore you decide to go back to the main town and prepare for war" << endl;
 				callArt.creatures();
-				//quest2();
+				//quest2(); 
+				// uncomment to start second quest- ready to be called
 			}
 			else if (choice_33 == 2) {
 				cout << "You try to go back to the village however night falls to dark , you could try again however it is 12 hours since next light." << endl;
         cout << "please pick from the options again" << endl;
 				goto attack_choice;
-        
 			}
 		}
 	
@@ -219,10 +200,19 @@ int choice_11 = 0, choice_22, choice_33,age;
 	}
 }
 
-
-
 int main(){
   out_town();
+  cout <<"welcome back you are now with in the main town."<<endl;
   
-  //in order to run the code calling it from a main function, its void as i extracted it from the main code.
+  // below to be called at the end of the code
+  
+  cout << "Askii art has been used from" << endl;
+  cout << "https://www.asciiart.eu/" << endl;
+  cout << "huge thanks to them!!"<<endl;
+cout <<"Code creaters"<<endl;
+cout<<"1.sunny - out of town\n2.luke main methods\n3.Hoa-lam - Combat sections\n4.hamaza & cameron & junaid databases";
+return 0;
 }
+  
+  //called from a main function all methods to to be called from main 
+  //void Function - performs task all methods to be stored in void
